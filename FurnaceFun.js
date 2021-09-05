fa86f1c502da1478acdd56bf009d9599ffc6c31b
the-electro-punk <bwr2555@hotmail.com>
2021-09-05 17:27:20 -0500

    fixed script

--- FurnaceFun.js
+++ FurnaceFun.js
@@ -1,5 +1,5 @@
 function randomNumber()
-// this is to create random nukmbers so I can randomize quiz answers
+// this is to create random numbers so I can randomize quiz answers
     for (i = 0; i < 20; i++)
     var NUMS =Math.floor(math.random(1) * 3);
     console.log(NUMS)
