0bd1339a7cf1944aefca8738c9932dc9749ebbb0
the-electro-punk <bwr2555@hotmail.com>
2021-09-05 22:13:05 -0500

    removed randomizer

--- FurnaceFun.js
+++
@@ -1,7 +0,0 @@
-function randomNumber()
-// this is to create random numbers so I can randomize quiz answers
-    for (i = 0; i < 20; i++)
-    var NUMS =Math.floor(math.random(1) * 3);
-    console.log(NUMS)
-
-randomNumber()
