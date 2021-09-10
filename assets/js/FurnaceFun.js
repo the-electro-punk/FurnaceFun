e043cc09600e832b54bf6847ba13c4e8f04946ae
the-electro-punk <bwr2555@hotmail.com>
2021-09-09 20:50:13 -0500

    added code to change text to questions and answers

--- assets/js/FurnaceFun.js
+++ assets/js/FurnaceFun.js
@@ -13,20 +13,20 @@ function startGame() {
 function questionStart() {
 
     // this selects a value so a random question is chosen from the array
-    var x = Math.floor(Math.random() * 10)
+    var x = Math.floor(Math.random() * 3)
     console.log('x = ' + x)
 // use a for loop for Math.random
-
+ 
      // this will decide how many steps in the array to take to select the next question/ this allows the questions to be a (mostly) random order
-    var y = Math.floor(Math.random() * 6) + 1
-    console.log('y = ' + y)
+    // var y = Math.floor(Math.random() * 6) + 1
+    // console.log('y = ' + y)
 
     // this changes the question text into one of the questions from the array
     let questionElement = document.getElementById('question').textContent = questArray[x]
-    let First = document.getElementById("btn-1").textContent = answersOne[x]
-    let Second = document.getElementById("btn-2").textContent = answersTwo[x]
-    let Third = document.getElementById("btn-3").textContent = answersThree[x]
-    
+    let First = document.getElementById('btn-1').textContent = answersOne[x].text
+    let Second = document.getElementById('btn-2').textContent = answersTwo[x].text
+    let Third = document.getElementById('btn-3').textContent = answersThree[x].text
+        
     // these were to test if the input was being made
     console.log(questionElement)
     console.log(First)
@@ -36,11 +36,16 @@ function questionStart() {
 
 // this array contains all the question so that the questionStart() function can choose a question based on a random number and the position of the question
 
-var questArray = ["This bird from Greek mythology is usually associated with fire and rising from the ashes after death?", "Kitsune are Japanese yokai that resemble which animal?", "Cerberus of Greek mythology had how many heads?", "What makes a Unicorn different from a regular horse?", "the Leviathan is associated with which environment?", "these dragons tend to have long slender bodies and lack wings.", "which greek monster had snakes for hair?", "this winged serpent was a god to the aztecs.", "which yokai is a female spirit associated with ice and snow?", "this jackal headed Egyptian diety was associated with mummification"]
+const questArray = ["This bird from Greek mythology is usually associated with fire and rising from the ashes after death?", "Kitsune are Japanese yokai that resemble which animal?", "Cerberus of Greek mythology had how many heads?", "What makes a Unicorn different from a regular horse?", "the Leviathan is associated with which environment?", "these dragons tend to have long slender bodies and lack wings.", "which greek monster had snakes for hair?", "this winged serpent was a god to the aztecs.", "which yokai is a female spirit associated with ice and snow?", "this jackal headed Egyptian diety was associated with mummification"]
 
 // these arrays contain the answers to insert into the answer buttons
 // var answersOne = ["Harpy", "Foxes", "Two", "Wings", "Land", "Eastern Dragons", "Sirens", "Questzalcoatl", "Yuki-Onna", "Anubis"]
 // var answersTwo = ["Phoenix", "Leopards", "Three", "Horn", "Air", "Western Dragons", "Gorgons", "Tezcatlipoca", "Rokurokubi", "Ra"]
 // var answersThree = ["Cucco", "Falcons", "Four", "Fire Breath", "Sea", "Smaugs", "Nagas", "Tonatiuh", "Jorogumo", "Horus"]
 
-var answers = [{"Harpy", "Phoenix", "Cucco"}]
+// these arrays contain the answers that will be speciifed to each button
+const answersOne = [{text: "Harpy", correct: false}, {text: "Foxes", correct: true}, {text: "Two", correct: false}]
+
+const answersTwo = [{text: "Phoenix", correct: true}, {text: "Leopards", correct: false}, {text: "Three", correct: true}]
+
+const answersThree = [{text: "Cucco", correct: false}, {text: "Falcons", correct: false}, {text: "Four", correct: false}]
